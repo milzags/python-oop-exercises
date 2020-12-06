fc9b797513bf8b728bc4186f853a7d95beec61fe
milanzagorac <zagoracm@gmail.com>
2020-12-06 09:54:46 -0500

    second part of assignment

--- wordfinder.py
+++ wordfinder.py
@@ -4,17 +4,14 @@ import random
 class WordFinder:
     
     def __init__(self, filepath):
-        """ read a file and show number of lines read """
+        """ read a file and show number of lines read;
+        create a list of words to be accessed in other 
+        methods """
         words_from_file = open(filepath, 'r')
         self.wordscount = 0
         self.wordslist = words_from_file.readlines()
         for line in self.wordslist: self.wordscount += 1
         print(f"{self.wordscount} words read")
-
-    def create_list(self, words_from_file):
-        """ create a list of words after reading the file"""
-        wordslist = [word for word in words_from_file.readlines()]
-        return wordslist
 
     def random_word(self):
         """ return a random word"""
@@ -26,9 +23,16 @@ rando_word = wf.random_word()
 print(rando_word)
 
 
-class RandomWordFinter(WordFinder):
+class RandomWordFinder(WordFinder):
     """ new randomWOrdFiner requirement: some lines will be blank
     and other lines will start with #; write code to handle this
     and re-use as much code as possible from earlier """
     
+    #basically write this so that it doesn't include blanks
+    #or lines that start with #
+    def check_for_valid_string(self, words_from_file):
+        if self.wordslist and not word.startswith('#'):
+            return words_from_file.readlines()
 
+valid_string = wf.check_for_valid_string()
+print(valid_string)
